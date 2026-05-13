8996c25de0f251b34e6f16af765284c060919d8b
VisaaganG <343505582@tdsb.ca>
2026-05-13 14:17:30 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,42 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static int findValue(int n, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int findThirdValue(String n, String[] arr) {
+        int count = 0;
 
-	}
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(n)) {
+                count++;
+
+                if (count == 3) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+	public static double[] append(double n, double[] arr) {
+        double[] newArr = new double[arr.length + 1];
+
+        
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        
+        newArr[arr.length] = n;
+
+        return newArr;
+    }
+
 
 }
